b9b519f8f37ecb4221079a21425eaf5f9ad38b9f
okifirsyah404 <okifirsyah404@gmail.com>
2022-10-09 09:55:55 +0700

    Update tugas

---
+++ lib/routes/route_generator.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../screens/about_screen.dart';
+import '../screens/home_screen.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/about':
+        return MaterialPageRoute(builder: (_) => AboutPage());
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(title: Text("Error")),
+        body: Center(child: Text('Error page')),
+      );
+    });
+  }
+}
---
+++ lib/screens/about_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tentang Aplikasi'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Kembali'),
+        ),
+      ),
+    );
+  }
+}
